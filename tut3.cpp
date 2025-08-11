ed4f736e55b3065b9252f4842910daa4df8dfc31
Arya Sekhar Roy <royarya431@gmail.com>
2025-08-11 19:49:12 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+    // int a =3;
+    // int b =3;
+    int a=14 , b=15;
+    cout<<"this is tut 4.\n Here the value of a is "<<a<<".\n The value of b is "<< b;
+    float e=2.76;
+    cout<<"\nthis is the value of e to the nearest two decimals "<<e;
+    char c ='f';
+    cout<<"\nthe value of c is "<<c;
+    bool is_true = true;
+    cout<<is_true;
+    return 0;
+}
